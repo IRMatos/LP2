7b58adbdf92cae7391a8f8343006b0ba4b5df781
IRMatos <48076749+IRMatos@users.noreply.github.com>
2021-03-21 19:23:05 -0300

    Update Rect.java

--- Classes 2.1/3.2/Figures/Rect.java
+++ Classes 2.1/3.2/Figures/Rect.java
@@ -3,14 +3,17 @@ package Figures;
 import java.awt.*;
 
 public class Rect {
-    int x, y;
-    int w, h;
+    private int x, y;
+    private int w, h;
+	private Color bgc, cdb;
 
-    public Rect (int x, int y, int w, int h) {
+    public Rect (int x, int y, int w, int h, Color cdb, Color bgc) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+		this.cdb = cdb;
+		this.bgc = bgc;
     }
 
     public void print () {
@@ -19,7 +22,12 @@ public class Rect {
     }
 
     public void paint (Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(cdb);
         g2d.drawRect(this.x,this.y, this.w,this.h);
+		this.x+=1;this.y+=1;
+		this.w-=1;this.h-=1;
+		g2d.setColor(bgc);
+		g2d.fillRect(this.x,this.y, this.w,this.h);
     }
 }
