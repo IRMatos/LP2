95ce98fddd40a74fc353fcfffe92daafd2a88fca
IRMatos <48076749+IRMatos@users.noreply.github.com>
2021-05-29 18:59:25 -0300

    Update Rect.java

--- Projeto 1/figures/Rect.java
+++ Projeto 1/figures/Rect.java
@@ -4,35 +4,26 @@ import java.awt.*;
 
 public class Rect extends Figure {
 
-    public Rect (int x, int y, int w, int h, Color cordefundo, Color contorno) {
-        super(x, y, w, h, cordefundo, contorno);
+    public Rect (int x, int y, int w, int h, int cfr, int cfg, int cfb, int ccr, int ccg, int ccb) {
+        super(x, y, w, h, cfr, cfg, cfb, ccr, ccg, ccb);
     }
 
-    public void print () {
+    private void print () {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.w, this.h, this.x, this.y);
     }
 
-    @Override
-
-    public void paint (Graphics g) {
+    public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRect(this.x,this.y, this.w,this.h);
-        g2d.setStroke(new BasicStroke(3));
-
-        g2d.setColor(this.cordefundo);
-        g2d.fillRect(this.x, this.y, this.w, this.h);
-
-        g2d.setColor(this.contorno);
-        g2d.drawRect(this.x, this.y, this.w, this.h);
-    }
-
-    public void redFocus (Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-
-        g2d.setStroke(new BasicStroke(1));
-
-        g2d.setColor(Color.yellow);
-        g2d.drawRect(this.x, this.y, this.w, this.h);
+		
+	if (focused) {
+	    g2d.setPaint(Color.BLUE);
+	    g2d.drawRect(this.x-3, this.y-3, this.w+6, this.h+6);
+	}
+		
+	g2d.setColor(new Color(this.cfr,this.cfg,this.cfb));
+	g2d.fillRect(this.x, this.y, this.w, this.h);
+    g2d.setColor(new Color(this.ccr,this.ccg,this.ccb));
+    g2d.drawRect(this.x, this.y, this.w, this.h);
     }
 }
