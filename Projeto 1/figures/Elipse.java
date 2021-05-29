33645bd7ec79ab6e4996579ba0683789d456acc8
IRMatos <48076749+IRMatos@users.noreply.github.com>
2021-05-29 18:59:57 -0300

    Update Elipse.java

--- Projeto 1/figures/Elipse.java
+++ Projeto 1/figures/Elipse.java
@@ -1,29 +1,31 @@
 package figures;
 
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Ellipse2D.Double;
+import java.awt.geom.*;
 
-public class Elipse extends Figure {
-
-    public Elipse (int x, int y, int w, int h, Color cordefundo, Color contorno) {
-        super(x, y, w, h, cordefundo, contorno);
+public class Ellipse extends Figure {
+	
+    public Ellipse (int x, int y, int w, int h, int cfr, int cfg, int cfb, int ccr, int ccg, int ccb) {
+        super(x, y, w, h, cfr, cfg, cfb, ccr, ccg, ccb);
     }
 
-    public void print () {
-        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+    private void print () {
+        System.out.format("Ellipse de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.w, this.h, this.x, this.y);
     }
 
-    @Override
-    public void paint (Graphics g) {
+    public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-
-        g2d.setColor(this.cordefundo);
-        g2d.fill(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
-
-        g2d.setColor(this.contorno);
-        g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+	
+	if (focused) {
+	    g2d.setPaint(Color.BLUE);
+	    g2d.drawOval(this.x-3, this.y-3, this.w+6, this.h+6);
+	}
+	
+	g2d.setColor(new Color(this.cfr,this.cfg,this.cfb));
+	g2d.fillOval(this.x,this.y, this.w,this.h);
+    g2d.drawOval(this.x,this.y, this.w,this.h);
+    g2d.setColor(new Color(this.ccr,this.ccg,this.ccb));
+	g2d.drawOval(this.x,this.y, this.w,this.h);
     }
 }
