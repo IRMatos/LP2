149551578172730127725a50e4db6fca28d2048c
IRMatos <48076749+IRMatos@users.noreply.github.com>
2021-05-29 18:58:36 -0300

    Update Figure.java

--- Projeto 1/figures/Figure.java
+++ Projeto 1/figures/Figure.java
@@ -1,31 +1,53 @@
-package ivisible;
 package figures;
 
-import java.awt.Graphics;
-import java.awt.*;
 
-public abstract class Figure {
-    public int x, y, w, h ; 
-    public Color cordefundo,contorno;
+import ivisible.IVisible;
+import java.io.Serializable;
 
-    public Figure (int x, int y,int w, int h, Color cordefundo, Color contorno) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.contorno = contorno;
-        this.cordefundo = cordefundo;
+public abstract class Figure implements IVisible, Serializable {
+    public int x, y;
+    public int w, h;
+    public int cfr,cfg,cfb;
+    public int ccr,ccg,ccb;
+	
+public Figure (int x, int y, int w, int h,int cfr, int cfg, int cfb,int ccr, int ccg, int ccb){
+    this.x = x;
+    this.y = y;
+	this.w = w;
+	this.h = h;
+    this.cfr = cfr;
+    this.cfg = cfg;
+	this.cfb = cfb;
+    this.ccr = ccr;
+    this.ccg = ccg;
+	this.ccb = ccb;
+ }
+	
+    public void drag (int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+    
+    public void resize (int dw, int dh) {
+	    this.w += dw;
+	    this.h += dh;
+    }
+	
+    public void fundo (int cfr, int cfg, int cfb) {
+	    this.cfr = cfr;
+	    this.cfg = cfg;
+	    this.cfb = cfb;
     }
 
-    public abstract void paint (Graphics g);
+    public void contorno (int ccr, int ccg, int ccb) {
+        this.ccr = ccr;
+        this.ccg = ccg;
+        this.ccb = ccb;
+        }
 
-    public void drag (int dx, int dy){
-        x += dx - x;
-        y += dy -y;
+	
+    public boolean clicked (int x, int y) {
+        return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
     }
-
 }
 
-public boolean clicked (int x, int y) {
-      return (this.x<=x && x<=this.x+this.w && this.y<=y && y<=this.y+this.h);
-    }
